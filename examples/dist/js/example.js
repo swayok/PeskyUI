4f4edf96738ed9c7a8a03ef6d847c0c3c65601c4
Filippov Alexander <swayalex.work@gmail.com>
2021-01-07 20:05:53 +0300

    Added UI color pickers to right sidebar

--- examples/dist/js/example.js
+++ examples/dist/js/example.js
@@ -1,4 +1,5 @@
 $(function () {
+    window.LoadedData = {};
     var $app = $('#app');
     Eta.configure({
         //async: true,
@@ -13,6 +14,8 @@ $(function () {
             $.get('templates/sidebar_left_menu.html'),
             $.get('templates/sidebar_left_menu_item.html'),
             $.getJSON('data/sidebar_left_menu.json'),
+            $.get('templates/sidebar_right_ui_colors_picker.html'),
+            $.getJSON('data/ui_colors.json'),
         )
         .done(async function (
             layout,
@@ -23,15 +26,19 @@ $(function () {
             footer,
             sbLeftMenu,
             sbLeftMenuItem,
-            sbLeftMenuData
+            sbLeftMenuData,
+            sbRightUiColorsPicker,
+            sbRightUiColorsData
         ) {
             Eta.templates.define('sidebar-left-menu-item', Eta.compile(sbLeftMenuItem[0]));
             Eta.templates.define('sidebar-left-menu', Eta.compile(sbLeftMenu[0]));
             Eta.templates.define('sidebar-left', Eta.compile(sbLeft[0]));
+            Eta.templates.define('sidebar-right-ui-colors-picker', Eta.compile(sbRightUiColorsPicker[0]));
             Eta.templates.define('sidebar-right', Eta.compile(sbRight[0]));
             Eta.templates.define('topbar', Eta.compile(topBar[0]));
             Eta.templates.define('footer', Eta.compile(footer[0]));
             Eta.templates.define('content', Eta.compile(content[0]));
+            window.LoadedData.uiColors = sbRightUiColorsData[0];
             $app.html(Eta.render(layout[0], {
                 sidebarLeftMenuItems: sbLeftMenuData[0]
             }));
